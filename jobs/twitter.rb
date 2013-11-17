c163edda8743b783c9c8fa8fe2dafeb1b5f3ef4d
Sven Mueller <sven.mueller@commercetools.de>
2013-11-17 15:36:16 +0100

    Adjust twitter settings.

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -4,13 +4,13 @@ require 'twitter'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 Twitter.configure do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.oauth_token = 'YOUR_OAUTH_TOKEN'
-  config.oauth_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key = 'QEeTIoGMlhs50bw2jxlNKA'
+  config.consumer_secret = ''
+  config.oauth_token = '14401202-cXLxsGhUrFs52Pww2LFBWd93L0OwhP77Kwk5AV9XI'
+  config.oauth_token_secret = ''
 end
 
-search_term = URI::encode('#todayilearned')
+search_term = URI::encode('#commercetools OR @commercetools OR #sphereio OR @sphereio')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
