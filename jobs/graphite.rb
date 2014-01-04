81451361fb07a771efd6dc752f986c2362a10e54
Sven Mueller <sven.mueller@commercetools.de>
2014-01-04 22:30:33 +0100

    Show list with orders per store in  descending order.

--- jobs/graphite.rb
+++ jobs/graphite.rb
@@ -66,3 +66,29 @@ SCHEDULER.every '30s', :first_in => 0 do
     # send to dashboard, so the number the meter and the graph widget can understand it
     send_event 'errors', { current: current, value: current, points: points, graphcolor: "#fff"}
 end
+
+
+stores = ['airfield', 'backjoy', 'bootstrap', 'brita', 'cdl', 'cpr', 'fbh', 'ggbailey', 'kela', 'kfr', 'qspex', 'rbcollection', 'redbull', 'rollingrock', 'ser', 'tiggers']
+
+SCHEDULER.every '30s', :first_in => 0 do
+
+    orders = Array.new    #=> []
+
+    # Create an instance of our helper class
+    q = Graphite.new url
+
+    stores.each do |store|
+
+        name = "integral(sumSeries(nonNegativeDerivative(servers.de.commercetools.prod.grid.puppetmaster.commercetools.orders.#{store}.*.*.*.count)))"
+
+        # get total value
+        current = q.value name, "-24h"
+        orders.push({ label: store, value: current })
+    end
+
+    orders = orders.sort_by {|record| record[:value]}
+    send_event('orders', { items: orders.reverse! })
+end
+
+
+
